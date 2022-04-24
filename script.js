d10e5997f9b67eb208f90fb5041cb520192238fd
ayush815 <103514684+ayush815@users.noreply.github.com>
2022-04-24 22:33:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     if(principal<=0){
-        alert("Enter a positive amount");
+        alert("Enter a Positive Number");
     }
     else{
         interest;
